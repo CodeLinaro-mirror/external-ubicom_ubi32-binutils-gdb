ac41604910388c28ecbb5763b19d794baf604d8c
Michael Eager <eager@eagercon.com>
2016-03-23 11:14:01 -0700

    GDB - Preliminary support for Qualcomm Ubi32.
    This patch adds basic definitions and support for the Qualcomm Ubi32
    processor.  Enables loading and disassembly of ubi32 object files and
    executables.  Does not include inferior (e.g. target remote) support.
    
    * ubi32-tdeb.c: New.
    * ubi32-tdep.h: New.

---
+++ gdb/ubi32-tdep.c
@@ -0,0 +1,230 @@
+/* Target-dependent code for Qualcomm UBI32.
+
+   Copyright (C) 2016 Free Software Foundation, Inc.
+
+   This file is part of GDB.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+
+#include "defs.h"
+#include "arch-utils.h"
+#include "target-descriptions.h"
+#include "ubi32-tdep.h"
+#include "dis-asm.h"
+#include "opcodes/ubi32-dis.h"
+
+/* Register names of the Qualcomm UBI32 V6/V6.1 processor.  */
+static const char *ubi32_register_names[] =
+{
+  /* General data registers.  */
+  "d0", "d1", "d2",  "d3",  "d4",  "d5",  "d6",  "d7",	/*  0- 7 */
+  "d8", "d9", "d10", "d11", "d12", "d13", "d14", "d15",	/*  8-15 */
+  /* Address registers.  */
+  "a0", "a1", "a2",  "a3",  "a4",  "a5",  "a6",  "sp",	/* 16-23 */
+  /* Accumulator 0.  */
+  "acc0_hi", "acc0_lo", "mac_rc16", "source_3", 	/* 24-27 */
+  /* Context & Control.  */
+  "context_cnt", "csr", "rosr", "iread_data",		/* 28-31 */
+  /* Interrupt mask registers.  */
+  "int_mask0", "int_mask1", "int_mask2", "int_mask3", 	/* 32-35 */
+  /* Program counter & trap.  */
+  "pc", "rap_cause",					/* 36-37 */
+  /* Accumulator 1.  */
+  "acc1_hi", "acc1_lo", 				/* 38-39 */
+  /* Previous PC & Control register.  */
+  "previous_pc", "ucsr",				/* 40-41 */
+  /* Chip ID.  */
+  "chip_id",						/* 42    */
+  /* Interrupt status, set, and clear.  */
+  "int_stat0", "int_stat1", "int_stat2", "int_stat3",	/* 43-46 */
+  "int_set0", "int_set1", "int_set2", "int_set3",	/* 47-50 */
+  "int_clr0", "int_clr1", "int_clr2", "int_clr3",	/* 51-54 */
+  /* Global control.  */
+  "global_ctrl",					/* 55	 */
+  /* Threads active status, set, and clear.  */
+  "mt_active", "mt_active_set", "mt_active_clr",	/* 56-58 */
+  /* Debugging treads active status and set.  */
+  "mt_dbg_active", "mt_dbg_active_set",			/* 59-60 */
+  /* Thread control enable, priority, scheduling.  */
+  "mt_en", "mt_pri", "mt_sched", 			/* 61-63 */
+  /* Thread stopped in break and clear.  */
+  "mt_break", "mt_break_clr",				/* 64-65 */
+  /* Single-step threads.  */
+  "mt_single_step",					/* 66	 */
+  /* Threads delay scheduling.  */
+  "mt_min_del_en",					/* 67	 */
+  /* Thread break set.  */
+  "mt_break_set",					/* 68	 */
+  /* Data capture address.  */
+  "dcapt",						/* 69	 */
+  /* Debugging threads active status clear.  */
+  "mt_dbg_active_clr",					/* 70	 */
+  /* Scratchpad registers.  */
+  "scratchpad0", "scratchpad1", "scratchpad2",		/* 71-73 */
+  "scratchpad3", "scratchpad4", "scratchpad5",		/* 74-76 */
+  "scratchpad6",					/* 77	 */
+  /* Chip configuration.  */
+  "chip_cfg",						/* 78	 */
+  /* Thread status and trap control.  */
+  "mt_i_blocked", "mt_d_blocked", 			/* 79-80 */
+  "mt_i_blocked_set", "mt_d_blocked_set",		/* 81-82 */
+  "mt_blocked_clr", "mt_trap_en",			/* 83-84 */
+  "mt_trap", "mt_trap_set", "mt_trap_clr",		/* 85-87 */
+  /* Supervisor Call Entry Point.  */
+  "sep",						/* 88	 */
+  /* Valid instruction address ranges high, low, enable.  */
+  "i_range0_hi", "i_range1_hi", "i_range2_hi", "i_range3_hi", /* 89-92 */
+  "i_range0_lo", "i_range1_lo", "i_range2_lo", "i_range3_lo", /* 93-96 */
+  "i_range0_en", "i_range1_en", "i_range2_en", "i_range3_en", /* 97-100 */
+  /* Valid data address ranges high, low, enable.  */
+  "d_range0_hi", "d_range1_hi", "d_range2_hi", "d_range3_hi",
+  "d_range4_hi", "d_range5_hi",				/* 101-106 */
+  "d_range0_lo", "d_range1_lo", "d_range2_lo", "d_range3_lo",
+  "d_range4_lo", "d_range5_lo",				/* 107-112 */
+  "d_range0_en", "d_range1_en", "d_range2_en", "d_range3_en",
+  "d_range4_en", "d_range5_en",				/* 113-118 */
+  /* Valid user instruction address ranges enable.  */
+  "i_range0_user_en", "i_range1_user_en",
+  "i_range2_user_en", "i_range3_user_en",		/* 119-122 */
+  /* Valid user data address ranges enable.  */
+  "d_range0_user_en", "d_range1_user_en",
+  "d_range2_user_en", "d_range3_user_en",
+  "d_range4_user_en", "d_range5_user_en",		/* 123-128 */
+  /* FP control registers.  */
+  "mt_fp_flush", "mt_fp_blocked", "mt_fp_blocked_set",	/* 129-131 */
+  /* BTB control registers.  */
+  "mt_btb_en", "btb_ctrl",				/* 132-133 */
+};
+#define UBI32_NUM_REGS ARRAY_SIZE (ubi32_register_names)
+
+/* Return the name of register regnum.  */
+static const char *
+ubi32_register_name (struct gdbarch *gdbarch, int regnum ATTRIBUTE_UNUSED)
+{
+  if (regnum >= 0 && regnum < UBI32_NUM_REGS)
+    return ubi32_register_names[regnum];
+  return NULL;
+}
+
+static struct type *
+ubi32_register_type (struct gdbarch *gdbarch, int num )
+{
+  return builtin_type (gdbarch)->builtin_int;
+}
+
+static CORE_ADDR
+ubi32_skip_prologue (struct gdbarch *gdbarch, CORE_ADDR start_pc)
+{
+  printf ("ubi32_skip_prologue() not implemented\n");
+  gdb_assert (0);
+  return 0;
+}
+
+/* Return an instruction to set a breakpoint at PCPTR, adjusting PCPTR if
+   necessary, and store in LENPTR the size of the returned instruction.  */
+
+static const unsigned char *
+ubi32_breakpoint_from_pc (struct gdbarch *gdbarch, CORE_ADDR *pc,
+			  int *len)
+{
+  /* BKPT instruction.  */
+  static unsigned char break_insn[] = { 0x00, 0x00, 0x38, 0x00 };
+
+  *len = sizeof (break_insn);
+  return break_insn;
+}
+
+static struct gdbarch *
+ubi32_gdbarch_init (struct gdbarch_info info, struct gdbarch_list *arches)
+{
+  struct gdbarch *gdbarch;
+  struct tdesc_arch_data *tdesc_data = NULL;
+  const struct target_desc *tdesc = info.target_desc;
+  struct gdbarch_tdep *tdep;
+  int i;
+
+  /* Find a candidate among extant architectures.  Only one architecture so
+     far, so use simple search. */
+  arches = gdbarch_list_lookup_by_info (arches, &info);
+  if (arches != NULL)
+    {
+      return arches->gdbarch;
+    }
+
+  /* Check any target description for validity.  */
+  if (tdesc_has_registers (tdesc))
+    {
+      const struct tdesc_feature *feature;
+      int valid_p;
+
+      feature = tdesc_find_feature (tdesc, "org.gnu.gdb.ubi32.core");
+      if (feature == NULL)
+        return NULL;
+      tdesc_data = tdesc_data_alloc ();
+
+      valid_p = 1;
+      for (i = 0; i < UBI32_NUM_REGS; i++)
+        valid_p &= tdesc_numbered_register (feature, tdesc_data, i,
+                                            ubi32_register_names[i]);
+      if (!valid_p)
+        {
+          tdesc_data_cleanup (tdesc_data);
+          return NULL;
+        }
+    }
+
+  /* Allocate space for the new architecture.  */
+  tdep = XNEW (struct gdbarch_tdep);
+  gdbarch = gdbarch_alloc (&info, tdep);
+
+  set_gdbarch_num_regs (gdbarch, UBI32_NUM_REGS);
+  set_gdbarch_register_name (gdbarch, ubi32_register_name);
+  set_gdbarch_register_type (gdbarch, ubi32_register_type);
+
+  /* Register numbers of stack pointer and program counter.  */
+  set_gdbarch_sp_regnum (gdbarch, UBI32_SP_REGNUM);
+  set_gdbarch_pc_regnum (gdbarch, UBI32_PC_REGNUM);
+
+  /* Set data sizes.  */
+  set_gdbarch_num_pseudo_regs (gdbarch, 0);
+  set_gdbarch_ptr_bit (gdbarch, 32);
+  set_gdbarch_short_bit (gdbarch, 16);
+  set_gdbarch_int_bit (gdbarch, 32);
+  set_gdbarch_long_bit (gdbarch, 32);
+  set_gdbarch_long_long_bit (gdbarch, 64);
+  set_gdbarch_float_bit (gdbarch, 32);
+  set_gdbarch_double_bit (gdbarch, 64);
+  set_gdbarch_long_double_bit (gdbarch, 64);
+  set_gdbarch_float_format (gdbarch, floatformats_ieee_single);
+  set_gdbarch_double_format (gdbarch, floatformats_ieee_double);
+  set_gdbarch_long_double_format (gdbarch, floatformats_ieee_double);
+
+  set_gdbarch_skip_prologue (gdbarch, ubi32_skip_prologue);
+  set_gdbarch_inner_than (gdbarch, core_addr_lessthan);
+  set_gdbarch_breakpoint_from_pc (gdbarch, ubi32_breakpoint_from_pc);
+
+  set_gdbarch_print_insn (gdbarch, print_insn_ubi32);
+
+  return gdbarch;
+}
+
+/* Silence -Wmissing-prototypes */
+initialize_file_ftype _initialize_ubi32_tdep;
+
+void
+_initialize_ubi32_tdep (void)
+{
+  register_gdbarch_init (bfd_arch_ubi32, ubi32_gdbarch_init);
+}
---
+++ gdb/ubi32-tdep.h
@@ -0,0 +1,64 @@
+/* Target-dependent code for Qualcomm Ubi32.
+
+   Copyright (C) 2016 Free Software Foundation, Inc.
+
+   This file is part of GDB.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+
+/* Target-dependent structure in gdbarch.  */
+struct gdbarch_tdep
+{
+  /* No target-dependent data currently defined.  */
+};
+
+
+enum ubicom32v6_gdb_regnum {
+  UBI32_D0_REGNUM = 0,				/* general data */
+  UBI32_D1_REGNUM = 1,				/* general data */
+  UBI32_D2_REGNUM = 2,				/* general data */
+  UBI32_D3_REGNUM = 3,				/* general data */
+  UBI32_D4_REGNUM = 4,				/* general data */
+  UBI32_D5_REGNUM = 5,				/* general data */
+  UBI32_D6_REGNUM = 6,				/* general data */
+  UBI32_D7_REGNUM = 7,				/* general data */
+  UBI32_D8_REGNUM = 8,				/* general data */
+  UBI32_D9_REGNUM = 9,				/* general data */
+  UBI32_D10_REGNUM = 10,			/* general data */
+  UBI32_D11_REGNUM = 11,			/* general data */
+  UBI32_D12_REGNUM = 12,			/* general data */
+  UBI32_D13_REGNUM = 13,			/* general data */
+  UBI32_D14_REGNUM = 14,			/* general data */
+  UBI32_D15_REGNUM = 15,			/* general data */
+  UBI32_A0_REGNUM = 16,				/* general address */
+  UBI32_A1_REGNUM = 17,				/* general address */
+  UBI32_A2_REGNUM = 18,				/* general address */
+  UBI32_A3_REGNUM = 19,				/* general address */
+  UBI32_A4_REGNUM = 20,				/* general address */
+  UBI32_A5_REGNUM = 21,				/* general address / link register */
+  UBI32_A6_REGNUM = 22,				/* general address / frame pointer  */
+  UBI32_A7_REGNUM = 23,				/* gerenal address / stack pointer */
+  UBI32_SP_REGNUM = UBI32_A7_REGNUM,		/* stack pointer */
+  UBI32_LR_REGNUM  = UBI32_A5_REGNUM,		/* link register (return address) */
+  UBI32_PC_REGNUM = 36,				/* program counter */
+  UBI32_RET_REGNUM = UBI32_D0_REGNUM,		/* return value */
+  UBI32_RET2_REGNUM = UBI32_D1_REGNUM,		/* 2nd word of 8-byte return value */
+  UBI32_SRET_REGNUM = UBI32_D0_REGNUM,		/* returned struct address */
+  UBI32_ROSR_REGNUM = 30,			/* read-only status */
+  UBI32_MT_ACTIVE_REGNUM = 52,			/* threads active status */
+  UBI32_MT_EN_REGNUM = 57,			/* threads enabled */
+  UBI32_MT_PRI_REGNUM = 58,			/* thread priorities */
+  UBI32_MT_SCHED_REGNUM = 59,			/* thread scheduling policies */
+};
