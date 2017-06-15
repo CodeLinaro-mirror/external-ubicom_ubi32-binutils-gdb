824fcbfdd6f86f5c90d5c70d203e5fa01dd7d90b
Michael Eager <eager@eagercon.com>
2017-06-15 10:33:53 -0700

    GDB - Build fix for C++
    * gdb/ubi32-tdep.c(ubi32_skip_prologue): Use std::max() instead of max().

--- gdb/ubi32-tdep.c
+++ gdb/ubi32-tdep.c
@@ -473,7 +473,7 @@ ubi32_skip_prologue (struct gdbarch *gdbarch, CORE_ADDR start_pc)
       CORE_ADDR post_prologue_pc
 	= skip_prologue_using_sal (gdbarch, func_addr);
       if (post_prologue_pc != 0)
-	return max (start_pc, post_prologue_pc);
+	return std::max (start_pc, post_prologue_pc);
     }
 
 #if 0
